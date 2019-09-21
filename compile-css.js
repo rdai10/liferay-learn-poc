cda28a38a23e4ab8ab6138bb15e611f542f74411
Rebecca Dai <rhdai10@gmail.com>
2019-09-20 17:37:22 -0700

    add header

--- compile-css.js
+++ compile-css.js
@@ -1,14 +1,11 @@
 const fs = require('fs');
-const path = require('path');
 const sass = require('node-sass');
 
 const clayCss = require('clay-css');
 
-fs.readdirSync('source/_static/scss/').forEach(file => {
-	const data = sass.renderSync({
-		data: fs.readFileSync(path.join('source/_static/scss/', file), 'utf8'),
-		includePaths: [clayCss.includePaths]
-    });
-
-	fs.writeFileSync('source/_static/main.css', data.css);
+const data = sass.renderSync({
+	file: 'source/_static/scss/main.scss',
+	includePaths: [clayCss.includePaths]
 });
+
+fs.writeFileSync('source/_static/main.css', data.css);
