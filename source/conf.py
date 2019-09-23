ce76d48318ca115d307474e1353fb81b55521128
Rebecca Dai <rhdai10@gmail.com>
2019-09-23 12:10:33 -0700

    move toctree to contents.rst and create index.html

--- source/conf.py
+++ source/conf.py
@@ -35,12 +35,14 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['node_modules']
+exclude_patterns = ['node_modules', 'build']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 source_suffix = ['.rst', '.md']
+
+master_doc = 'contents'
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -66,6 +68,10 @@ html_show_sphinx = False
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_additional_pages = {
+    'index': 'index.html'
+}
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
